c9ccb1472153bc0a45925a6985d4da4331991889
Chris Eun <eunchristopher@gmail.com>
2023-01-25 02:49:35 -0800

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -8,6 +8,12 @@ function error404 () {
         <main>
             <h1>404: PAGE NOT FOUND</h1>
             <p>Oops, sorry, we can't find this page!</p>
+            <div>
+               <img src="./images/404.jpg" alt="error 404 image" />
+            </div>
+            <div>
+              Photo by <a href="https://unsplash.com/@niuhang?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">hang niu</a> on <a href="https://unsplash.com/images/animals/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            </div>
         </main>
     </Def>
   )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,13 @@ function home () {
     <Def>
         <main>
             <h1>HOME</h1>
+            <div>
+              <img src="./images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+            </div>
+            <div>
+              Photo by <a href="https://unsplash.com/es/@crystalweed?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">CRYSTALWEED cannabis</a> on <a href="https://unsplash.com/photos/10DTLp1jKf0?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+            </div>
             <a href="/places">
                 <button className="btn-primary">Places Page</button>
             </a>
