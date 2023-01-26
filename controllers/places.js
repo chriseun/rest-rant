85870d6c60e76106123841eae900d16e7ba6b9de
Chris Eun <eunchristopher@gmail.com>
2023-01-25 19:34:47 -0800

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -2,32 +2,29 @@ const router = require('express').Router()
 const places = require('../models/places.js')
 
 
-// router.get('/', (req, res) =>{
-//   res.send('GET /places')
-// })
-
 
 router.get('/', (req, res) => {
-//   let places = [{
-//   name: 'H-Thai-ML',
-//   city: 'Seattle',
-//   state: 'WA',
-//   cuisines: 'Thai, Pan-Asian',
-//   pic: '/images/restaurant-1.jpg'
-// }, {
-//   name: 'Coding Cat Cafe',
-//   city: 'Phoenix',
-//   state: 'AZ',
-//   cuisines: 'Coffee, Bakery',
-//   pic: '/images/restaurant-2.jpg'
-// }]
-
   res.render('places/index', { places })
 })
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
+
 
 router.post('/', (req, res) => {
   // console.log(req.body)
